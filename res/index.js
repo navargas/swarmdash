6fa966c3c19f365746354c92f137f00d5486d756
Nicholas Vargas <mail@nickvargas.net>
2015-09-21 03:23:10 +0000

    Enable loading from mqtt updates

--- res/index.js
+++ res/index.js
@@ -16,6 +16,20 @@ var exapmle = {
 $(function () {
   var clientIdInput  = $('#clientIdInput');
   var clientIdButton = $('#clientIdButton');
+  var hasFirstEvent = false;
+
+  function subscribe(id, client) {
+    client.subscribe(
+      'iot-2/type/quickstart/id/' + id + '/evt/event/fmt/json'
+    );
+  };
+
+  var appClientConfig = {
+    org: 'quickstart',
+    type: 'quickstart',
+    id: 'virtualdevice-104248025367916554634-default'
+  };
+  var iotClient = new IBMIoTF.ApplicationClient(appClientConfig);
   
   // User submits client ID
   clientIdButton.click(function(e) {
@@ -24,10 +38,24 @@ $(function () {
     clientIdButton.css('background-color', 'rgba(255,255,255,0)');
     clientIdButton.css('border-color', 'rgba(255,255,255,0)');
     clientIdButton.blur();
+    var readyTest = setInterval(function() {
+      console.log('Waiting for data...');
+      if (!hasFirstEvent) return;
+      clearInterval(readyTest);
+      $('.clientid').hide();
+      $('#dashbox').css('display', 'block');
+    }, 1000);
+    subscribe(clientIdInput.val(), iotClient);
+    location.hash = clientIdInput.val();
   });
 
-  $('.clientid').hide();
-  $('#dashbox').css('display', 'block');
+
+  function switchToDash() {
+    if (!hasFirstEvent) return;
+    clearInterval(this);
+    $('.clientid').hide();
+    $('#dashbox').css('display', 'block');
+  };
 
   function newColor(seed) {
     selection = ['A', 'F', 'B', 'E', 'C', 'D'];
@@ -38,9 +66,10 @@ $(function () {
     return '#' + R + G + B;
   };
 
-  function loadJSON(object) {
-    for (var provider in object) {
-      if (!object.hasOwnProperty(provider)) continue;
+  function loadJSON(cluster) {
+    $('#dashbox').empty();
+    for (var provider in cluster) {
+      if (!cluster.hasOwnProperty(provider)) continue;
       var title = $('<h3>')
         .text(provider);
       var item = $('<div>')
@@ -51,15 +80,35 @@ $(function () {
       var box = $('<span>')
         .addClass('col-md-4 col-sm-6')
         .append(item);
-      var innerItems = object[provider];
-      for (var machine in innerItems) {
+      for (var machine in cluster[provider]) {
+        var list = $('<ul>');
         var innerChild = $('<div>')
           .addClass('col-md-12')
-          .addClass('innerContainer');
-        innerChild.text(machine);
+          .addClass('innerContainer')
+          .text(machine)
+          .append(list);
+        for (var container in cluster[provider][machine]) {
+          var innermostItem = $('<li>')
+            .text(cluster[provider][machine][container]);
+          list.append(innermostItem);
+        }
         item.append(innerChild);
       }
       $('#dashbox').append(box);
     }
-  }; loadJSON(exapmle);
+  };
+
+  iotClient.on('connect',function(e) {
+    if (location.hash) {
+      clientIdInput.val(location.hash.slice(1));
+      clientIdButton.trigger('click');
+    };
+  });
+  iotClient.on('deviceEvent',function(e) {
+    hasFirstEvent = true;
+    loadJSON(JSON.parse(e.payload.toString()));
+  });
+  iotClient.on('error',function(e) {console.log(e)});
+  iotClient.connect();
+
 });
